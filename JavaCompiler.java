feffdd872feb8f8a827c43edcbec762361a81699
dyriusdev <richardsondlopes@hotmail.com>
2025-03-08 00:30:34 -0300

    Fixing java output

--- JavaCompiler.java
+++ JavaCompiler.java
@@ -1,4 +1,4 @@
-import java.io.BufferedReader;
+    import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,29 +6,49 @@ import java.util.List;
 public class JavaCompiler {
 
     public static void compile(String file) throws Exception {
-        List<String> args = new ArrayList<>();
-        args.add("javac");
-        args.add(file);
+        String className = file.substring(file.lastIndexOf("\\") + 1, file.lastIndexOf("."));
+        List<String> argsCompilation = new ArrayList<>();
+        argsCompilation.add("javac");
+        argsCompilation.add(file);
 
-        ProcessBuilder builder = new ProcessBuilder(args);
-        Process process = builder.start();
+        ProcessBuilder compilationBuilder = new ProcessBuilder(argsCompilation);
+        Process compilationProcess = compilationBuilder.start();
 
-        BufferedReader out = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String outLine;
-        while ((outLine = out.readLine()) != null) {
-            System.out.println(outLine);
+        BufferedReader out = new BufferedReader(new InputStreamReader(compilationProcess.getInputStream()));
+        String outComp;
+        while ((outComp = out.readLine()) != null) {
+            System.out.println(outComp);
         }
 
-        BufferedReader errors = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-        while ((outLine = errors.readLine()) != null) {
-            System.err.println(outLine);
+        BufferedReader errorsCompilation = new BufferedReader(new InputStreamReader(compilationProcess.getErrorStream()));
+        while ((outComp = errorsCompilation.readLine()) != null) {
+            System.err.println(outComp);
         }
 
-        int exitCode = process.waitFor();
-        if (exitCode == 0) {
-            System.out.println("Compiled successfully");
-        } else {
-            System.err.println("Compilation failed");
+        int code = compilationProcess.waitFor();
+        if (code != 0) {
+            System.err.println("Compilação falhou.");
+            return;
         }
+
+        List<String> argsExec = new ArrayList<>();
+        argsExec.add("java");
+        argsExec.add(className);
+
+        ProcessBuilder execBuilder = new ProcessBuilder(argsExec);
+        Process processExec = execBuilder.start();
+
+        BufferedReader outExec = new BufferedReader(new InputStreamReader(processExec.getInputStream()));
+        String lineExec;
+        while ((lineExec = outExec.readLine()) != null) {
+            System.out.println(lineExec);
+        }
+
+        BufferedReader errorsExec = new BufferedReader(new InputStreamReader(processExec.getErrorStream()));
+        while ((lineExec = errorsExec.readLine()) != null) {
+            System.err.println(lineExec);
+        }
+
+        processExec.waitFor();
     }
 }
